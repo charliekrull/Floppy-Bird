24f5d3baed585b5d75cc82ae4fa86c4fdc3ae9cf
Charlie Krull <charlesckrull@gmail.com>
2022-07-10 21:21:57 -0400

    Added pause feature

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -25,75 +25,82 @@ function PlayState:init()
 end
 
 function PlayState:update(dt)
-    --update timer for pipe spawning
-    self.timer = self.timer + dt
-
-
-    --spawn a new pipe pair every second and a half
-
-    if self.timer > self.timeToNext then
-        --modify the last Y coordinate we placed so pipe gaps aren't impossible
-        --no higher than 10 pixels below top edge of screen,
-        --no lower than 90 pixels from the bottom
-        local y = math.max(-PIPE_HEIGHT + 10,
-        math.min(self.lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-        self.lastY = y
-
-        --add a new pipe pair at the end of the screen at our
-        table.insert(self.pipePairs, PipePair(y))
-
-        --reset timer
-        self.timer = 0
-        
-        --update timeToNext, between 1 and 2.5 seconds
-        self.timeToNext = math.random(1.5, 2.5)
+    
+    if love.keyboard.wasPressed('p') then
+        scrolling = not scrolling
     end
+    
+    if scrolling then
+        --update timer for pipe spawning
+        self.timer = self.timer + dt
 
-    for k, pair in pairs(self.pipePairs) do
-        --score a point if its right edge is beyond the bird's left edge
-        --ignore if already been scored
-        if not pair.scored then
-            if pair.x + PIPE_WIDTH < self.bird.x then
-                self.score = self.score + 1
-                pair.scored = true
-                sounds['score']:play()
+
+        --spawn a new pipe pair every second and a half
+
+        if self.timer > self.timeToNext then
+            --modify the last Y coordinate we placed so pipe gaps aren't impossible
+            --no higher than 10 pixels below top edge of screen,
+            --no lower than 90 pixels from the bottom
+            local y = math.max(-PIPE_HEIGHT + 10,
+            math.min(self.lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+            self.lastY = y
+
+            --add a new pipe pair at the end of the screen at our
+            table.insert(self.pipePairs, PipePair(y))
+
+            --reset timer
+            self.timer = 0
+            
+            --update timeToNext, between 1 and 2.5 seconds
+            self.timeToNext = math.random(1.5, 2.5)
+        end
+
+        for k, pair in pairs(self.pipePairs) do
+            --score a point if its right edge is beyond the bird's left edge
+            --ignore if already been scored
+            if not pair.scored then
+                if pair.x + PIPE_WIDTH < self.bird.x then
+                    self.score = self.score + 1
+                    pair.scored = true
+                    sounds['score']:play()
+                end
+            end
+
+            pair:update(dt)
+        end
+
+        --remove any flagged pipes
+        --we need this second loop because modifying the table in place would cause skipping pipePairs
+        for k, pair in pairs(self.pipePairs) do
+            if pair.remove then
+                table.remove(self.pipePairs, k)
             end
         end
 
-        pair:update(dt)
-    end
+        self.bird:update(dt)
 
-    --remove any flagged pipes
-    --we need this second loop because modifying the table in place would cause skipping pipePairs
-    for k, pair in pairs(self.pipePairs) do
-        if pair.remove then
-            table.remove(self.pipePairs, k)
-        end
-    end
+        --simple collision between bird andall pipes in pairs
+        for k, pair in pairs(self.pipePairs) do
+            for l, pipe in pairs(pair.pipes) do
+                if self.bird:collides(pipe) then
+                    sounds['explosion']:play()
+                    sounds['hurt']:play()
 
-    self.bird:update(dt)
-
-    --simple collision between bird andall pipes in pairs
-    for k, pair in pairs(self.pipePairs) do
-        for l, pipe in pairs(pair.pipes) do
-            if self.bird:collides(pipe) then
-                sounds['explosion']:play()
-                sounds['hurt']:play()
-
-                gStateMachine:change('score', {
-                    score = self.score
-                })
+                    gStateMachine:change('score', {
+                        score = self.score
+                    })
+                end
             end
         end
-    end
-    --reset if bird hits the ground
-    if self.bird.y > VIRTUAL_HEIGHT - 15 then
-        sounds['explosion']:play()
-        sounds['hurt']:play()
+        --reset if bird hits the ground
+        if self.bird.y > VIRTUAL_HEIGHT - 15 then
+            sounds['explosion']:play()
+            sounds['hurt']:play()
 
-        gStateMachine:change('score', {
-            score = self.score
-        })
+            gStateMachine:change('score', {
+                score = self.score
+            })
+        end
     end
 
 end
